074f47abd738d99772cd2a2e74be2fa42d3355bf
Èric Vega Noguera <120464037+ericveganoguera@users.noreply.github.com>
2023-06-14 12:50:57 +0200

    Delete web-page directory

--- web-page/src/pages/AboutMe.js
+++
@@ -1,7 +0,0 @@
-export default function AboutMe () {
-    return (
-        <div>
-            <h1>AboutMe</h1>
-        </div>
-    )
-}
--- web-page/src/pages/HomePage.js
+++
@@ -1,7 +0,0 @@
-export default function HomePage () {
-    return (
-        <div>
-            <h1>HOMEPAGE</h1>
-        </div>
-    )
-}
